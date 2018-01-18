3d43ecb8e7c40dd8668a1c7d45f66e897be36093
matheus-henr <matheushenrique.dev@gmail.com>
2018-01-18 20:32:59 -0200

    Projeto adicionado

---
+++ src/main/java/br/com/locaVeiculos/locadora/LocadoraDeVeiculosApplication.java
@@ -0,0 +1,12 @@
+package br.com.locaVeiculos.locadora;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LocadoraDeVeiculosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LocadoraDeVeiculosApplication.class, args);
+	}
+}
